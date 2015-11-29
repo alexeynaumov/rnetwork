3687281c03710711ffc8ef858a750a08d085452b
Alexei Naumov <rocketbuzzz@gmail.com>
2015-11-29 18:45:33 +0300

    Pre-release fixes done.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name="rnetwork",
-      version = "1.0.1",
-      author = "Alexei Naumov",
-      author_email = "alexei.naumov@yahoo.com",           
+      version = "1.1.1",
+      author = "Alexey Naumov",
+      author_email = "rocketbuzzz@gmail.com",           
       description = "The rnetwork package makes network programming and debugging easy.",
-      packages =  ["rnetwork", "dbg/tcp/tcpdebugger"],
-      scripts = ["dbg/dbg"]
+      packages =  ["rnetwork", "dbg", "dbg/tcp", "dbg/tcp/client", "dbg/tcp/server", "dbg/tcp/tcpdebugger"],
+      scripts = ["dbg/tcp/tdbg"]
      )
