f806965f4df269cf9b5e2c3e7823f97411509f0b
Alexey Naumov <rocketbuzzz@gmail.com>
2016-04-23 18:07:26 +0300

    setup.py fixed

--- setup.py
+++ setup.py
@@ -4,7 +4,15 @@ setup(name="rnetwork",
       version = "1.1.1",
       author = "Alexey Naumov",
       author_email = "rocketbuzzz@gmail.com",           
-      description = "The rnetwork package makes network programming and debugging easy.",
-      packages =  ["rnetwork", "dbg", "dbg/tcp", "dbg/tcp/client", "dbg/tcp/server", "dbg/tcp/tcpdebugger"],
-      scripts = ["dbg/tcp/tdbg"]
+      description = "",
+      packages =  ["rnetwork", "ndbg", "ndbg/tcp", "ndbg/tcp/client", "ndbg/tcp/server", "ndbg/udp"],
+      package_data = {
+          'ndbg/tcp/client': [
+              'icons/*',
+          ],
+          'ndbg/tcp/server': [
+              'icons/*',
+          ],
+      },
+      scripts = ["ndbg/tcp/tdbgc", "ndbg/tcp/tdbgs"]
      )
